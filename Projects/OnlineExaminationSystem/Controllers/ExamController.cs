68d22e9a21110ae902c92dd5849eaff0d3f3eba5
Your Name <riazdev18@gmail.com>
2025-01-19 10:01:48 +0500

    adding User's views

--- Projects/OnlineExaminationSystem/Controllers/ExamController.cs
+++ Projects/OnlineExaminationSystem/Controllers/ExamController.cs
@@ -4,7 +4,6 @@ using OnlineExaminationSystem.Models;
 namespace OnlineExaminationSystem.Controllers
 {
     public class ExamController : Controller
-
     {
         private readonly IExamRepository _examRepository;
 
@@ -13,11 +12,90 @@ namespace OnlineExaminationSystem.Controllers
             _examRepository = examRepository;
         }
 
+        // Get all exams
         public IActionResult ExamInfo()
         {
             var exams = _examRepository.GetAllExams();
             return View(exams);
         }
-       
+
+        // Get exam details by ID
+        public IActionResult Details(int id)
+        {
+            var exam = _examRepository.GetExamById(id);
+            if (exam == null)
+            {
+                return NotFound();
+            }
+            return View(exam);
+        }
+
+        // Create a new exam (GET)
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // Add a new exam (POST)
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Exam exam)
+        {
+            if (ModelState.IsValid)
+            {
+                _examRepository.AddExam(exam); // Calls the AddExam method of IExamRepository
+                return RedirectToAction(nameof(ExamInfo)); // Redirect back to ExamInfo view
+            }
+            return View(exam);
+        }
+
+        // Edit an exam (GET)
+        public IActionResult Edit(int id)
+        {
+            var exam = _examRepository.GetExamById(id);
+            if (exam == null)
+            {
+                return NotFound();
+            }
+            return View(exam);
+        }
+
+        // Edit an exam (POST)
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(int id, Exam exam)
+        {
+            if (id != exam.ExamId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                _examRepository.UpdateExam(exam);
+                return RedirectToAction(nameof(ExamInfo));
+            }
+            return View(exam);
+        }
+
+        // Delete an exam (GET)
+        public IActionResult Delete(int id)
+        {
+            var exam = _examRepository.GetExamById(id);
+            if (exam == null)
+            {
+                return NotFound();
+            }
+            return View(exam);
+        }
+
+        // Delete an exam (POST)
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeleteConfirmed(int id)
+        {
+            _examRepository.DeleteExam(id);
+            return RedirectToAction(nameof(ExamInfo));
+        }
     }
 }
