ec37c5f343ee075af9a5e51be6aa0059d8a52f93
Your Name <riazdev18@gmail.com>
2025-01-26 12:36:51 +0500

    build Docker Image

---
+++ Practice/SimpleConsoleApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello! This is a simple C# console application.");
+        Console.WriteLine("What is your name?");
+        string name = Console.ReadLine();
+        Console.WriteLine($"Welcome, {name}!");
+    }
+}
