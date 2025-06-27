869491051226f15fa2b309730f64779e1c3008a6
Your Name <riazdev18@gmail.com>
2025-06-27 10:23:34 +0500

    Rearrange the folder strucure

---
+++ Practice/ArrayIndexing/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace ArrayIndexing
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] randomNo = new int[100];
+            Random random = new Random();
+            for (int i = 0; i < randomNo.Length; i++) {
+                randomNo[i] = random.Next(60,100);
+            }
+             
+            for(int j=0; j<randomNo.Length; j++)
+            {
+                if (randomNo[j] == 67)
+                {
+                    Console.WriteLine("index of 67 is found at: " + j);
+                }  
+            }
+           
+            Console.ReadLine();
+        }
+    }
+}
